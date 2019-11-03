3e7bdfe200180a5375c965abbd5b55a6ab132c6f
Kristjan Reba <kristjan.reba96@gmail.com>
2019-11-03 21:05:54 +0100

    added training for all 3 algorithms

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 import gym
 
+from stable_baselines import results_plotter
 from stable_baselines import A2C, SAC, PPO2
 from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.sac.policies import MlpPolicy as SacMlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 
 import tensorflow as tf
@@ -9,28 +11,32 @@ import tensorflow as tf
 tf.logging.set_verbosity(tf.logging.ERROR)
 
 
+def render_env(env, model):
+    obs = env.reset()
+    for i in range(1000):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        env.render()
+    env.close()
 
-# Define and train a model in one line of code !
-trained_model = A2C('MlpPolicy', 'CartPole-v1').learn(total_timesteps=10000)
-# you can then access the gym env using trained_model.get_env()
-trained_model.get_env()
-print('Finished 1')
 
+if __name__ == '__main__':
 
+    env = gym.make('Pendulum-v0')
+    env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
 
+    model_A2C = A2C(MlpPolicy, env, verbose=1)
+    model_A2C.learn(total_timesteps=10000)
 
-env = gym.make('CartPole-v1')
-env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
+    model_SAC = SAC(SacMlpPolicy, env, verbose=1)
+    model_SAC.learn(total_timesteps=10000)
 
-model = A2C(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=10000)
+    model_PPO2 = PPO2(MlpPolicy, env, verbose=1)
+    model_PPO2.learn(total_timesteps=10000)
 
-obs = env.reset()
-for i in range(1000):
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
-    env.render()
+    render_env(env, model_A2C)
+    render_env(env, model_SAC)
+    render_env(env, model_PPO2)
 
-env.close()
+    results_plotter.main()
 
-print('Finished 2')
