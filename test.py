2fad234bd88f48feaaf82b27b62ed9a329a2ac10
Kristjan Reba <kristjan.reba96@gmail.com>
2019-11-05 21:19:34 +0100

    plotting changes

--- test.py
+++ test.py
@@ -1,42 +1,133 @@
+import os
 import gym
+import numpy as np
 
 from stable_baselines import results_plotter
 from stable_baselines import A2C, SAC, PPO2
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.sac.policies import MlpPolicy as SacMlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.bench import Monitor
+from stable_baselines.results_plotter import load_results, ts2xy
 
+import matplotlib.pyplot as plt
 import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 
 
-def render_env(env, model):
+def render_env(env, model, timestamps):
     obs = env.reset()
-    for i in range(1000):
+    for i in range(timestamps):
         action, _states = model.predict(obs)
         obs, rewards, dones, info = env.step(action)
         env.render()
     env.close()
 
 
+best_mean_reward, n_steps = -np.inf, 0
+
+def callback(_locals, _globals):
+  """
+  Callback called at each step (for DQN an others) or after n steps (see ACER or PPO2)
+  :param _locals: (dict)
+  :param _globals: (dict)
+  """
+  global n_steps, best_mean_reward
+  # Print stats every 1000 calls
+  if (n_steps + 1) % 1000 == 0:
+      # Evaluate policy training performance
+      x, y = ts2xy(load_results(log_dir), 'timesteps')
+      if len(x) > 0:
+          mean_reward = np.mean(y[-100:])
+          print(x[-1], 'timesteps')
+          print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(best_mean_reward, mean_reward))
+
+          # New best model, you could save the agent here
+          if mean_reward > best_mean_reward:
+              best_mean_reward = mean_reward
+              # Example for saving best model
+              print("Saving new best model")
+              _locals['self'].save(log_dir + 'best_model.pkl')
+  n_steps += 1
+  return True
+
+
+def moving_average(values, window):
+    """
+    Smooth values by doing a moving average
+    :param values: (numpy array)
+    :param window: (int)
+    :return: (numpy array)
+    """
+    weights = np.repeat(1.0, window) / window
+    return np.convolve(values, weights, 'valid')
+
+
+def plot_results(log_folder, agent_name, title='Learning Curve'):
+    """
+    plot the results
+
+    :param log_folder: (str) the save location of the results to plot
+    :param title: (str) the title of the task to plot
+    """
+    x, y = ts2xy(load_results(log_folder), 'timesteps')
+    y = moving_average(y, window=50)
+    # Truncate x
+    x = x[len(x) - len(y):]
+
+    fig = plt.figure(title)
+    plt.plot(x, y)
+    plt.xlabel('Number of Timesteps')
+    plt.ylabel('Rewards')
+    plt.title(title + " Smoothed")
+    plt.savefig(agent_name+".png")
+
+
 if __name__ == '__main__':
 
-    env = gym.make('Pendulum-v0')
-    env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
+    RENDER_TIMESTAMPS = int(1e3)
+    TRAIN_TIMESTAMPS = int(1e5)
+    env_name = 'BipedalWalker-v2'
 
-    model_A2C = A2C(MlpPolicy, env, verbose=1)
-    model_A2C.learn(total_timesteps=10000)
 
-    model_SAC = SAC(SacMlpPolicy, env, verbose=1)
-    model_SAC.learn(total_timesteps=10000)
+    agent_name = 'a2c'
+    print('Testing agent ' + agent_name)
+    best_mean_reward, n_steps = -np.inf, 0
+    log_dir = 'logging_' + agent_name + '/'
+    os.makedirs(log_dir, exist_ok=True)
+    env = gym.make(env_name)
+    env = Monitor(env, log_dir, allow_early_resets=True)
+    env = DummyVecEnv([lambda: env])
+    model_A2C = A2C(MlpPolicy, env, verbose=0)
+    model_A2C.learn(total_timesteps=TRAIN_TIMESTAMPS, callback=callback)
+    plot_results(log_dir, agent_name)
+ 
+    agent_name = 'sac'
+    print('Testing agent ' + agent_name)
+    best_mean_reward, n_steps = -np.inf, 0
+    log_dir = 'logging_' + agent_name + '/'
+    os.makedirs(log_dir, exist_ok=True)
+    env = gym.make(env_name)
+    env = Monitor(env, log_dir, allow_early_resets=True)
+    env = DummyVecEnv([lambda: env])
+    model_SAC = SAC(SacMlpPolicy, env, verbose=0)
+    model_SAC.learn(total_timesteps=TRAIN_TIMESTAMPS, callback=callback) 
+    plot_results(log_dir, agent_name)
 
-    model_PPO2 = PPO2(MlpPolicy, env, verbose=1)
-    model_PPO2.learn(total_timesteps=10000)
+    agent_name = 'ppo'
+    print('Testing agent ' + agent_name)
+    best_mean_reward, n_steps = -np.inf, 0
+    log_dir = 'logging_' + agent_name + '/'
+    os.makedirs(log_dir, exist_ok=True)
+    env = gym.make(env_name)
+    env = Monitor(env, log_dir, allow_early_resets=True)
+    env = DummyVecEnv([lambda: env])
+    model_PPO2 = PPO2(MlpPolicy, env, verbose=0)
+    model_PPO2.learn(total_timesteps=TRAIN_TIMESTAMPS, callback=callback)
+    plot_results(log_dir, agent_name)
 
-    render_env(env, model_A2C)
-    render_env(env, model_SAC)
-    render_env(env, model_PPO2)
-
-    results_plotter.main()
+    #render_env(env, model_A2C)
+    #render_env(env, model_SAC)
+    #render_env(env, model_PPO2)
 
